5e0db98d93c375e95a166745483ee9a9c74b7379
Enrique Ortiz <hi@enriqueortiz.dev>
2022-11-15 19:32:05 -0400

    feat: support drop tunings

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ const NOTES_DIM: [&str; 12] = [
     "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B",
 ];
 
+#[derive(Default)]
 enum Tuning {
+    #[default]
     StandardTuning,
     DropTuning,
 }
@@ -18,24 +20,25 @@ fn get_note_index(note: &str) -> usize {
         .expect("Note not found")
 }
 
-fn get_notes(root: &str) -> Vec<String> {
+fn get_notes(root: &str, tuning: &Tuning) -> Vec<String> {
     let mut notes = vec![root.to_string()];
 
-    // 5th string
-    notes.push(NOTES[((get_note_index(root) + 5) % 12)].to_string());
-    // 4th string
-    notes.push(NOTES[((get_note_index(notes.get(1).unwrap()) + 5) % 12)].to_string());
-    // 3rd string
-    notes.push(NOTES[((get_note_index(notes.get(2).unwrap()) + 5) % 12)].to_string());
-    // 2nd string (it's offset by one semitone)
-    notes.push(NOTES[((get_note_index(notes.get(3).unwrap()) + 4) % 12)].to_string());
-    // 1st string
-    notes.push(NOTES[((get_note_index(notes.get(4).unwrap()) + 5) % 12)].to_string());
+    for x in (1..6).rev() {
+        match (x, tuning) {
+            (5, Tuning::DropTuning) => {
+                notes.push(NOTES[((get_note_index(notes.last().unwrap()) + 7) % 12)].to_string())
+            }
+            (2, _) => {
+                notes.push(NOTES[((get_note_index(notes.last().unwrap()) + 4) % 12)].to_string())
+            }
+            _ => notes.push(NOTES[((get_note_index(notes.last().unwrap()) + 5) % 12)].to_string()),
+        }
+    }
 
     notes
 }
 
 fn main() {
-    let notes = get_notes("C");
+    let notes = get_notes("D", &Tuning::DropTuning);
     println!("{:?}", notes);
 }
